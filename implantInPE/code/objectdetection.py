65d04e5fa359b972360a90e07fe7aa1fa0441e8a
Runshi-Zhang <94819508+Runshi-Zhang@users.noreply.github.com>
2023-02-18 20:07:20 +0800

    Add files via upload

--- implantInPE/code/objectdetection.py
+++ implantInPE/code/objectdetection.py
@@ -1,4 +1,5 @@
 import pydicom as dicom
+import os
 import scipy.ndimage
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
@@ -8,6 +9,9 @@ from mmdet.apis import init_detector, inference_detector
 import os
 import cv2
 import json
+import time
+from scipy import misc
+
 
 def load_scan(path):
     slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
@@ -42,7 +46,7 @@ def get_pixels_hu(slices):
 
 
     return np.array(image, dtype=np.int16)
-def resample(image, scan, new_spacing=[0.5,0.5,0.5]):
+def resample(image, scan, new_spacing=[1,1,1]):
     # Determine current pixel spacing
     print(scan[0].PixelSpacing)
     #spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
@@ -58,38 +62,58 @@ def resample(image, scan, new_spacing=[0.5,0.5,0.5]):
 
     #image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
     image = scipy.ndimage.zoom(image, real_resize_factor, mode='nearest')
-
     return image, new_spacing, spacing
 
-#def ReData(image,scan,r):
-
-
-
+time1 = time.time()
 #load the CT data (dicom).
 first_patient = load_scan("")
 first_patient_pixels = get_pixels_hu(first_patient)
-
+# plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
+# plt.xlabel("Hounsfield Units (HU)")
+# plt.ylabel("Frequency")
+# plt.show()
 pix_resampled, spacing, oldspacing = resample(first_patient_pixels, first_patient)
+# print("Shape before resampling\t", first_patient_pixels.shape)
+# print("Shape after resampling\t", pix_resampled.shape)
+# tempPix = np.zeros((pix_resampled.shape[0],pix_resampled.shape[1],pix_resampled.shape[2] - 10))
+# tempPix[:,:,:] = pix_resampled[:,:,10:]
+# pix_resampled = tempPix
+
 
 # the radius of PCA processing
 r = 20
-centerIndex = int(pix_resampled.shape[2] / 2)
 
+centerIndex = int(pix_resampled.shape[2] / 2)
+'''
+arr_180 = pix_resampled[:, :, centerIndex][::-1, ...][:, ::-1]
+plt.imshow(arr_180, cmap=plt.cm.bone)
+'''
+# plt.show()
+# print(first_patient)
 
 firpatpix = pix_resampled[:, :, centerIndex - r:centerIndex + r]
 pcadata = firpatpix.reshape(firpatpix.shape[0] * firpatpix.shape[1], firpatpix.shape[2])
 
-pca = PCA(n_components=1)  # n_components can be integer or float in (0,1)
-pca.fit(pcadata)  # fit the model
-
+# n_components can be integer or float in (0,1)
+pca = PCA(n_components=1)  
+# fit the model
+pca.fit(pcadata)  
+# print('\nMethod 3: PCA by Scikit-learn:')
+# print('After PCA transformation, data becomes:')
 pcaResult = pca.fit_transform(pcadata)  # transformed data
 re = pcaResult.reshape(firpatpix.shape[0], firpatpix.shape[1])
-plt.imshow(re[::-1, ...][:, ::-1], cmap=plt.cm.bone)
-
+#plt.imshow(re[::-1, ...][:, ::-1], cmap=plt.cm.bone)
+# plt.show()
+'''
+mipdata = np.max(pcadata, axis=1)
+mipre = mipdata.reshape(firpatpix.shape[0], firpatpix.shape[1])
+plt.imshow(mipre[::-1, ...][:, ::-1], cmap=plt.cm.bone)
+'''
+# plt.show()
 imageSize = np.append([512], [512])
-print(re.shape)
+#print(re.shape)
 imageSize = imageSize / re.shape
-print(imageSize)
+#print(imageSize)
 
 pcaImage = scipy.ndimage.zoom(re, imageSize, mode='nearest')
 plt.imsave("./middle.jpg", pcaImage[::-1, ...][:, ::-1], cmap='gray')
@@ -100,11 +124,7 @@ checkpoint_file = './sparsercnn.pth'
 model = init_detector(config_file, checkpoint_file)  # or device='cuda:0'
 img = mmcv.imread('./middle.jpg')
 result = inference_detector(model, img)
-plt.imsave("./segimagetruth/" + str(i) + '.jpg',result,
+time2 = time.time()
+print(time2 - time1)
+plt.imsave(".jpg",result,
                    cmap='gray')
-
-
-
-
-
-
--- implantInPE/code/shapedesign .py
+++ implantInPE/code/shapedesign .py
@@ -11,6 +11,7 @@ import pycpd as pypd
 
 import os
 import matplotlib.pyplot as plt
+import time
 
 #PCD algorithm visualization
 def visualize(iteration, error, X, Y, ax):
@@ -25,167 +26,225 @@ def visualize(iteration, error, X, Y, ax):
     plt.draw()
     plt.pause(0.001)
 
-
-
 if __name__ == "__main__":
-     #load the segmentation module (cocoCascade.py and cascademaskrcnn.pth).
+    time1 = time.time()
+    #load the segmentation module (cocoCascade.py and cascademaskrcnn.pth).
     config_file = './cocoCascader100.py'
     checkpoint_file = './cascademaskrcnn.pth'
     model = init_detector(config_file, checkpoint_file)  # or device='cuda:0'
 
     #The file includes the axial slices of PE on the nearest distance to the DDP.
-    path_photox = './result/'  
-    files_listx = os.listdir(path_photox)
-    for aa in range(len(files_listx)):
-        imgX = mmcv.imread(path_photox + files_listx[aa])
-        resultX = inference_detector(model, imgX)
-        tempX = np.zeros((512, 512))
-        for i in range(len(resultX[1])):
-            for j in range(len(resultX[1][i])):
-                tempX = resultX[1][i][j] + tempX
+    path_photox = './result/0.jpg'  # 所有photo所在的文件夹目录
 
-        #Threshold segmentation was used to extract the contour of anterior chest.
-        colorX = np.zeros((512, 512, 3))
-        colorX = np.array(colorX, dtype='uint8')
-        colorX[:, :, 0] = tempX[:, :]
-        colorX[:, :, 1] = tempX[:, :]
-        colorX[:, :, 2] = tempX[:, :]
-        gray = cv2.cvtColor(colorX, cv2.COLOR_BGR2GRAY)
-        cv2.ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+    imgX = mmcv.imread(path_photox)
+    resultX = inference_detector(model, imgX)
+    tempX = np.zeros((512, 512))
+    for i in range(len(resultX[1])):
+        for j in range(len(resultX[1][i])):
+            tempX = resultX[1][i][j] + tempX
 
-        contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(imgX, contours, -1, (0, 0, 255), 3)
-        cv2.imshow("result", imgX)
+    #Threshold segmentation was used to extract the contour of anterior chest.
+    colorX = np.zeros((512, 512, 3))
+    colorX = np.array(colorX, dtype='uint8')
+    colorX[:, :, 0] = tempX[:, :]
+    colorX[:, :, 1] = tempX[:, :]
+    colorX[:, :, 2] = tempX[:, :]
+    gray = cv2.cvtColor(colorX, cv2.COLOR_BGR2GRAY)
+    cv2.ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(imgX, contours, -1, (0, 0, 255), 3)
+    cv2.imshow("result", imgX)
+    # cv2.waitKey(0)
+
+    posX1 = []
+    posX2 = []
+    for i in range(len(contours)):
+        for j in range(contours[i].shape[0]):
+            posX1.append(contours[i][j, 0, 0])
+            posX2.append(contours[i][j, 0, 1])
+
+    X = np.zeros((len(posX1), 2))
+    X[:, 0] = posX1[:]
+    X[:, 1] = posX2[:]
+
+    #The file includes the axial slices of the healthy thoracic cage.
+    path_photo = './test1/'  
+    files_list = os.listdir(path_photo)
+    q1 = 10000
+    dff = 1
+    for a in range(len(files_list)):
+        imgY = mmcv.imread(path_photo + files_list[a])
+        resultY = inference_detector(model, imgY)
+        tempY = np.zeros((512, 512))
+        for i in range(len(resultY[1])):
+            for j in range(len(resultY[1][i])):
+                tempY = resultY[1][i][j] + tempY
+        colorY = np.zeros((512, 512, 3))
+        colorY = np.array(colorY, dtype='uint8')
+        colorY[:, :, 0] = tempY[:, :]
+        colorY[:, :, 1] = tempY[:, :]
+        colorY[:, :, 2] = tempY[:, :]
+        grayY = cv2.cvtColor(colorY, cv2.COLOR_BGR2GRAY)
+        cv2.ret, binaryY = cv2.threshold(grayY, 0, 255, cv2.THRESH_BINARY)
+
+        contoursY, hierarchy = cv2.findContours(binaryY, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(imgY, contoursY, -1, (0, 0, 255), 3)
+        cv2.imshow("result", imgY)
         # cv2.waitKey(0)
 
-        posX1 = []
-        posX2 = []
-        for i in range(len(contours)):
-            for j in range(contours[i].shape[0]):
-                posX1.append(contours[i][j, 0, 0])
-                posX2.append(contours[i][j, 0, 1])
-
-        X = np.zeros((len(posX1), 2))
-        X[:, 0] = posX1[:]
-        X[:, 1] = posX2[:]
-
-        #The file includes the axial slices of the healthy thoracic cage.
-        path_photo = './test1/'  
-        files_list = os.listdir(path_photo)
-        q1 = 10000
-        dff = 1
-        for a in range(len(files_list)):
-            imgY = mmcv.imread(path_photo + files_list[a])
-            resultY = inference_detector(model, imgY)
-            tempY = np.zeros((512, 512))
-            for i in range(len(resultY[1])):
-                for j in range(len(resultY[1][i])):
-                    tempY = resultY[1][i][j] + tempY
-            colorY = np.zeros((512, 512, 3))
-            colorY = np.array(colorY, dtype='uint8')
-            colorY[:, :, 0] = tempY[:, :]
-            colorY[:, :, 1] = tempY[:, :]
-            colorY[:, :, 2] = tempY[:, :]
-            grayY = cv2.cvtColor(colorY, cv2.COLOR_BGR2GRAY)
-            cv2.ret, binaryY = cv2.threshold(grayY, 0, 255, cv2.THRESH_BINARY)
-
-            contoursY, hierarchy = cv2.findContours(binaryY, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(imgY, contoursY, -1, (0, 0, 255), 3)
-            cv2.imshow("result", imgY)
-            # cv2.waitKey(0)
-
-            posY1 = []
-            posY2 = []
-            for i in range(len(contoursY)):
-                for j in range(contoursY[i].shape[0]):
-                    posY1.append(contoursY[i][j, 0, 0])
-                    posY2.append(contoursY[i][j, 0, 1])
-            Y = np.zeros((len(posY1), 2))
-            Y[:, 0] = posY1[:]
-            Y[:, 1] = posY2[:]
-            q1 = 0
-            name = path_photo + files_list[a]
-            
-            #CPD
-            reg = pypd.RigidRegistration(**{'X': Y, 'Y': X})
-            # fig = plt.figure()
-            # fig.add_axes([0, 0, 1, 1])
-            # callback = partial(visualize, ax=fig.axes[0])
-            # reg.register(callback)
-            reg.register()
-            if (q1 > reg.q):
-                q1 = reg.q
-                name = files_list[a]
-                Ytemp = Y
+        posY1 = []
+        posY2 = []
+        for i in range(len(contoursY)):
+            for j in range(contoursY[i].shape[0]):
+                posY1.append(contoursY[i][j, 0, 0])
+                posY2.append(contoursY[i][j, 0, 1])
+        Y = np.zeros((len(posY1), 2))
+        Y[:, 0] = posY1[:]
+        Y[:, 1] = posY2[:]
+        q1 = 0
+        name = path_photo + files_list[a]
+        reg = pypd.RigidRegistration(**{'X': Y, 'Y': X})
+        # fig = plt.figure()
+        # fig.add_axes([0, 0, 1, 1])
+        # callback = partial(visualize, ax=fig.axes[0])
+        # reg.register(callback)
+        reg.register()
+        if (q1 > reg.q):
+            q1 = reg.q
+            name = files_list[a]
+            Ytemp = Y
             Y = Ytemp
 
-            reg = pypd.RigidRegistration(**{'X': Y, 'Y': X})
 
-            reg.register()
-            data, (s, r, t) = reg.register()
+        reg = pypd.RigidRegistration(**{'X': Y, 'Y': X})
 
-            print('s:', s)
-            print('R:\n', r)
-            print('t:\n', t)
+        reg.register()
+        data, (s, r, t) = reg.register()
 
-            xmin = np.argmin(Y, axis=0)
-            xmax = np.argmax(Y, axis=0)
-            if (Y[xmin[0], 1] < Y[xmax[0], 1]):
-                ymin = Y[xmax[0], 1]
-            else:
-                ymin = Y[xmin[0], 1]
+        print('s:', s)
+        print('R:\n', r)
+        print('t:\n', t)
 
-            print(xmax, '\n')
-            print(xmin, '\n')
-            extractX1 = []
-            extractY1 = []
-            for i in range(Y.shape[0]):
-                if (Y[i, 1] < ymin):
-                    extractX1.append(Y[i, 0])
-                    extractY1.append(Y[i, 1])
+        xmin = np.argmin(Y, axis=0)
+        xmax = np.argmax(Y, axis=0)
+        if (Y[xmin[0], 1] < Y[xmax[0], 1]):
+            ymin = Y[xmax[0], 1]
+        else:
+            ymin = Y[xmin[0], 1]
 
-            Y1 = np.zeros((len(extractX1), 2))
-            for i in range(len(extractX1)):
-                Y1[i, 0] = (extractX1[i] - t[0]) / s
-                Y1[i, 1] = (extractY1[i] - t[1]) / s
+        print(xmax, '\n')
+        print(xmin, '\n')
+        extractX1 = []
+        extractY1 = []
+        for i in range(Y.shape[0]):
+            if (Y[i, 1] < ymin):
+                extractX1.append(Y[i, 0])
+                extractY1.append(Y[i, 1])
 
-            Y2 = Y1.T
-            Ypre1 = np.linalg.solve(r.T, Y2)
-            Ypre = Ypre1.T
-            extractX = []
-            extractY = []
-            xmin = np.argmin(X, axis=0)
-            xmax = np.argmax(X, axis=0)
-            for i in range(len(extractX1)):
-                if (X[xmin[0], 0] < Ypre[i, 0]):
-                    if (X[xmax[0], 0] > Ypre[i, 0]):
-                        extractX.append(Ypre[i, 0])
-                        extractY.append(Ypre[i, 1])
+        Y1 = np.zeros((len(extractX1), 2))
+        for i in range(len(extractX1)):
+            Y1[i, 0] = (extractX1[i] - t[0]) / s
+            Y1[i, 1] = (extractY1[i] - t[1]) / s
 
-            points = []
-            ex = []
-            ey = []
-            sort1 = np.argsort(extractX)
-            for i in range(len(sort1)):
-                ex.append(extractX[sort1[i]])
-                ey.append(extractY[sort1[i]])
-            for i in range(len(extractY)):
-                points.append((int(ex[i]), int(ey[i])))
+            # Y1[i,0] = Y[i,0] / s - t[0]
+            # Y1[i, 1] = Y[i, 1] / s - t[1]
+        Y2 = Y1.T
+        Ypre1 = np.linalg.solve(r.T, Y2)
+        Ypre = Ypre1.T
+        extractX = []
+        extractY = []
+        xmin = np.argmin(X, axis=0)
+        xmax = np.argmax(X, axis=0)
+        for i in range(len(extractX1)):
+            if (X[xmin[0], 0] < Ypre[i, 0]):
+                if (X[xmax[0], 0] > Ypre[i, 0]):
+                    extractX.append(Ypre[i, 0])
+                    extractY.append(Ypre[i, 1])
 
-            p = tuple(points)
-            degree = 3
-            curve = fitting.approximate_curve(p, degree, ctrlpts_size=6)
+        # reg = pypd.AffineRegistration(**{'X':Y,'Y':X})
+        # data,(s,r,t) = reg.register()
+        '''
+        xmin = np.argmin(X, axis=0)
+        xmax = np.argmax(X, axis=0)
+        if (X[xmin[0], 1] < X[xmax[0], 1]):
+            ymin = X[xmax[0], 1]
+        else:
+            ymin = X[xmin[0], 1]
 
-            # Prepare points
-            evalpts = np.array(curve.evalpts)
-            pts = np.array(p)
+        print(xmax, '\n')
+        print(xmin, '\n')
+        extractX = []
+        extractY = []
+        for i in range(Ypre.shape[0]):
+            if (Ypre[i, 1] < ymin):
+                extractX.append(Ypre[i, 0])
+                extractY.append(Ypre[i, 1])
+        '''
 
-            # Plot points together on the same graph
-            fig = plt.figure()
-            plt.plot(evalpts[:, 0], 512 - evalpts[:, 1])
+        '''
+        # resultData = np.zeros()
 
-            plt.scatter(pts[:, 0], 512 - pts[:, 1], color="red", s=5)
+        fig = plt.figure()
+        plt.scatter(extractX[:], extractY[:])
+        plt.scatter(X[:, 0], X[:, 1])
+        plt.show()
+        # fitEllipse = []
+        # for i in range(len(extractX)):
+        #    fitEllipse.append((extractX[i],extractY[i]))
+        fitEllipse = np.zeros((len(extractX),2),dtype='int')
+        fitEllipse[:,0] = extractX[:]
+        fitEllipse[:,1] = extractY[:]
+        fitdata = np.zeros((512,512,3),dtype='uint8')
+        for i in range(len(extractX)):
+            fitdata[int(extractX[i]), int(extractY[i]),0] = 255
+            fitdata[int(extractX[i]), int(extractY[i]),1] = 0
+            fitdata[int(extractX[i]), int(extractY[i]),2] = 0
 
-            plt.scatter(X[:, 0], 512 - X[:, 1], color="blue", s=5)
+        #ell = cv2.fitEllipse(fitEllipse)
+        ell = cv2.minEnclosingCircle(fitEllipse)
+        cv2.ellipse(fitdata,ell,(0,0,255),2)
+        cv2.imshow("fit",fitdata)
+        cv2.waitKey(0)
+        with open('Ytest.txt', 'w') as f:
+            for i in range(len(extractX)):
+                f.write(str(extractX[i]) + '\t' + str(extractY[i]) + '\n')
+        with open('datatest.txt', 'w') as f1:
+            for i in range(data.shape[0]):
+                f1.write(str(data[i,0]) + '\t' + str(data[i,1]) + '\n')
+        '''
+        points = []
+        ex = []
+        ey = []
+        sort1 = np.argsort(extractX)
+        for i in range(len(sort1)):
+            ex.append(extractX[sort1[i]])
+            ey.append(extractY[sort1[i]])
+        for i in range(len(extractY)):
+            points.append((int(ex[i]), int(ey[i])))
 
+        p = tuple(points)
+        degree = 3
+        curve = fitting.approximate_curve(p, degree, ctrlpts_size=6)
+
+        # Prepare points
+        evalpts = np.array(curve.evalpts)
+        pts = np.array(p)
+        time2 = time.time()
+        print(time2 - time1)
+
+        # Plot points together on the same graph
+        fig = plt.figure()
+        plt.plot(evalpts[:, 0], 512 - evalpts[:, 1])
+
+        plt.scatter(pts[:, 0], 512 - pts[:, 1], color="red", s=5)
+
+        plt.scatter(X[:, 0], 512 - X[:, 1], color="blue", s=5)
+
+        # plt.scatter(Y[:, 0], Y[:, 1])
+        # plt.scatter(data[:, 0], data[:, 1])
+        # f = plt.gcf() 
+        # plt.imsave('./nonimg3/' + files_listx[aa][:-4] + 'u' + files_list[a][:-4] + 'q' + str(reg.q) + '.png')
+        # f.savefig('./nonimg2/' + files_listx[aa][:-4] + 'u' + files_list[a][:-4] + 'q' + str(reg.q) + '.png')
+        # f.clear() 
 
